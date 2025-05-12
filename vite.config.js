42bab5a974a792f8970bf95ec43a9b5cf416cf56
Shaked Braimok Yosef <shaked@senora.local>
2025-05-12 08:35:44 +0300

    fixed project name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/base44-app/',
+  base: '/slackbot-blueprint/',
   server: {
     allowedHosts: true
   },
